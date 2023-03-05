db09dced903308e9030bf3b4326e238f8133d5e8
Llorentee <116384586+Llorentee@users.noreply.github.com>
2023-03-05 18:52:04 +0300

    Add files via upload

---
+++ taksimetreHesaplama.java
@@ -0,0 +1,25 @@
+import java.util.Scanner;
+
+public class taksimetreHesaplama {
+    public static void main(String[] args) {
+        int km;
+        double kmbas = 2.20, total, startPrice = 10;
+        Scanner input = new Scanner(System.in);
+
+        System.out.print("Mesafeyi giriniz(km): ");
+        km = input.nextInt();
+
+        total = (km * kmbas);
+        total += startPrice;
+
+        total = (total < 20) ? 20 : total;
+
+        System.out.println("Toplam tutar: " + total);
+
+
+
+
+
+
+    }
+}
