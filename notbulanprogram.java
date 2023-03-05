7e9fdb73370114dcfc36e4bc1f9894492407bec7
Llorentee <116384586+Llorentee@users.noreply.github.com>
2023-03-05 18:32:46 +0300

    Add files via upload
     www.patika.dev

---
+++ notbulanprogram.java
@@ -0,0 +1,44 @@
+import java.util.Scanner;
+
+public class notbulanprogram {
+    public static void main(String[] args) {
+
+        int mat, fizik, kimya, turkce, tarih, muzik;
+
+        Scanner puan = new Scanner(System.in);
+
+        System.out.print("Matematik Notunuz : ");
+        mat = puan.nextInt();
+
+        System.out.print("Fizik Notunuz : ");
+        fizik = puan.nextInt();
+
+        System.out.print("Kimya Notunuz : ");
+        kimya = puan.nextInt();
+
+        System.out.print("Türkçe Notunuz : ");
+        turkce = puan.nextInt();
+
+        System.out.print("Tarih Notunuz : ");
+        tarih = puan.nextInt();
+
+        System.out.print("Müzik Notunuz : ");
+        muzik = puan.nextInt();
+
+
+        int toplam = (mat + fizik + kimya + tarih + turkce + muzik);
+        double ortalama = toplam / 6;
+        System.out.println(ortalama);
+
+        String sonuc = ortalama >= 6 ? "Sınıfı Geçti" : "Sınıfta kaldı";
+        System.out.print(sonuc);
+
+
+
+
+
+
+
+    }
+
+}
